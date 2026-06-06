ddce1e5204746eb95ce5c57cd45afb3775b85026
Sudipto Das <eng.sudiptodas@gmail.com>
2026-06-06 13:10:36 +0600

    setup: install React and Tailwind CSS configuration

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div >
+      <h1 className='text-9xl font-black text-center italic uppercase h-screen content-center text-transparent bg-clip-text bg-linear-to-r from-purple-400 to-pink-600'>Saffrona</h1>
+    </div>
+  )
+}
+
+export default App
